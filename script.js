a983f505f80b7e5354042efe2ea03335b89fe872
jonnytaddesky <johnsmith20532@gmail.com>
2019-02-03 22:43:48 +0200

    add new file and easy code

---
+++ script.js
@@ -0,0 +1,12 @@
+var person = {
+    name = "John",
+    age: 31,
+    favColor: "green",
+    height: 183
+};
+
+var x = person.age;
+var y = person['age'];
+
+document.write(x);
+document.write(y);
