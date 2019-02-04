372cd73d9e89f225d0cda5a20894c8fa16db2ea3
jonnytaddesky <sir.sivoglaz2012@gmail.com>
2019-02-04 14:09:34 +0200

    fixes path to js file

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 var person = {
-    name = "John",
+    name: "John",
     age: 31,
     favColor: "green",
     height: 183
@@ -8,5 +8,4 @@ var person = {
 var x = person.age;
 var y = person['age'];
 
-document.write(x);
-document.write(y);
+document.write(x + "<br \/>" + y);
